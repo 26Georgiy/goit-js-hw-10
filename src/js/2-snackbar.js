b8b0b1f47487ca88e98d96bfce6d62f58d63c8e9
26Georgiy <83900924+26Georgiy@users.noreply.github.com>
2024-01-24 21:12:48 +0100

    snackbar.js fixes
    1-deleted "is-shown" logic;
    2-fixed promise logic;

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -9,7 +9,7 @@ iziToast.settings({
   transitionOut: "flipOutX"
 });
 
-let isShown = false;
+// let isShown = false;
 const form = document.querySelector(".form");
 
 form.addEventListener("submit", promiseGenerator);
@@ -18,25 +18,17 @@ function promiseGenerator(e) {
   e.preventDefault();
   const delay = form.elements.delay.value;
   const state = form.elements.state.value;
-  // form reset with delay
-  if (!isShown) {
-    isShown = true;
-    iziToast.info({message: "Form will reset in 10 sec"});
-    setTimeout(() => {
-      iziToast.info({message: "Form reset..."});
-      form.reset();
-      isShown = false;
-    }, 10000);
-  }
 
   new Promise((resolve, reject) => {
     setTimeout(() => {
       if (state === "fulfilled") {
         resolve(delay);
       }
-      reject(delay);
+      else{
+      reject(delay);}
     }, delay);
+    form.reset();
   })
-    .then(value => iziToast.success({message: `Fulfilled promise in ${value}ms`}))
-    .catch(error => iziToast.error({message: `Rejected promise in ${error}ms`}));
+    .then(value => iziToast.success({message: `✅ Fulfilled promise in ${delay}ms`}))
+    .catch(error => iziToast.error({message: `❌ Rejected promise in ${delay}ms`}));
 }
